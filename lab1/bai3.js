b36b41a19aab8993b211e4c0630adca6e6a66640
tranvanchilinh1003 <chilinh140220@gmail.com>
2024-01-06 12:38:05 +0700

    lab1

---
+++ lab1/bai3.js
@@ -0,0 +1,35 @@
+'use strict'
+
+
+
+
+fetch('https://api.publicapis.org/entries').then(function(response){
+response.json().then(function(data){
+    let itemApi = '';
+    console.log(data);
+    const apiData = data.entries;
+    for(let i = 0; i < 15; i++){
+
+        itemApi += ` 
+        <tr>
+        <td>${i+1}</td>
+
+        <td>${apiData[i].API}</td>
+
+        <td>${apiData[i].Auth}</td>
+        <td>${apiData[i].Category}</td>
+        <td>${apiData[i].Description}</td>
+        <td>${apiData[i].Link}</td>
+         
+        </tr>  `;
+    }
+
+    document.getElementById('api3').innerHTML = itemApi;
+});
+
+})
+
+.catch(function(error){
+console.log(error);
+
+});
