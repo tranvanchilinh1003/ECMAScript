4f91c16df59dcd1851675ca452f8fde8a6c7c377
tranvanchilinh1003 <chilinh140220@gmail.com>
2024-01-25 13:21:28 +0700

    lab5

---
+++ Lab5/bai2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class Shape{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        console.log(`${this.x} ${this.y}`);
+    }
+}
+
+let a = new Shape("Hàm x", "Hàm y");
